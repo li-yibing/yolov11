418063502d787c32b2e0d5ec676cb74d902dab0a
ybli <li_yi_bing@outlook.com>
2025-06-05 00:05:29 +0800

    fix: 更新多线程

--- predict.py
+++ predict.py
@@ -1,12 +1,15 @@
 import cv2
 from ultralytics import YOLO
+from multiprocessing import freeze_support
 
-# Load a pretrained YOLO11n model
-model = YOLO("yolo11n.pt")
+if __name__ == '__main__':
+    freeze_support()    # 在 Windows 上运行多进程必需
+    # Load a pretrained YOLO11n model
+    model = YOLO("yolo11n.pt")
 
-# Define path to the image file
-source = "./images/su7.png"
+    # Define path to the image file
+    source = "./images/su7.png"
 
-# Run inference on the source
-results = model.predict(source, save=True, show=True)
-cv2.waitKey()
+    # Run inference on the source
+    results = model.predict(source, save=True, show=True)
+    cv2.waitKey()
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from ultralytics import YOLO
+from multiprocessing import freeze_support
 
-# Load a model
-model = YOLO("runs/detect/train3/weights/best.pt")  # TODO 指向自己训练的模型
+if __name__ == '__main__':
+    freeze_support()    # 在 Windows 上运行多进程必需
+    # Load a model
+    model = YOLO("runs/detect/train33/weights/best.pt")  # TODO 指向自己训练的模型
 
-# Customize validation settings
-metrics = model.val()
-results = model.predict("datasets/images/test", save=True)
+    # Customize validation settings
+    metrics = model.val()
+    results = model.predict("datasets/test/images", save=True, workers=1, conf=0.25, iou=0.65, save_txt=True, save_conf=True)
